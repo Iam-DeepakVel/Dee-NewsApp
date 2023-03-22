f40c6381cda4473c904355a0c958b4cb757aa4e9
iam-deepakvel <deepakvel82@gmail.com>
2023-03-22 11:17:38 +0530

    Feat: Dee-NewsApp Prototype v1.0

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p>{`Looks like this page doesn't exists`}</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
